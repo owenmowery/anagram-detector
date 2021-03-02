ff93d411dfb29c78c76550c41e1c1b80cdd28633
Owen Mowery <owenmowery99@gmail.com>
2021-03-02 16:57:04 -0500

    commit tree 823cecea2e14f882cc5369ce615ea6efec8f0546

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+
+class Anagram
+    attr_accessor :word, :reverse_array
+
+    def initialize(word)
+        @word = word
+        @reverse_array = @word.reverse.split("").sort
+    end
+
+    def match(words_array)
+        result = []
+        words_array.each do |words|
+            if words.split("").sort == @reverse_array
+                result << words
+            end
+        end
+        return result
+    end
+end
+
+listen = Anagram.new("listen")
+puts listen.match(%w(enlists google inlets banana))
